2a258d1c2ab7b986d4f376f6aa48f25ce2c57867
jane mckenna <janemckenna@hotmail.com>
2023-08-30 17:13:04 +0000

    inital commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// var xhr = new XMLHttpRequest();
+
+// xhr.onreadystatechange = function(){
+//     if (this.readyState == 4 && this.status == 200){
+//         document.getElementById('data').innerHTML = this.responseText;
+//     }
+// };
+
+// xhr.open('GET', 'https://ci-swapi.herokuapp.com/api/');
+
+// xhr.send();
+
+
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
